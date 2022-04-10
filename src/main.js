631af826dbcf368d0b4d370f128e2ccf28376baf
Andy <amillion3@gmail.com>
2022-04-09 22:23:01 -0400

    init commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import 'vuetify/styles' // Global CSS has to be imported
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+	.use(createPinia())
+	.use(router)
+  .mount('#app')
