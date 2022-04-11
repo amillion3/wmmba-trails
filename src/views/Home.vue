53e01d216541afd9a4470671dfdbde6c1e362c8b
Andy <amillion3@gmail.com>
2022-04-10 21:19:11 -0400

    added and updated basic home view with responsiveness

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container fluid class="fill-height">
+    <v-row>
+      <v-col cols="12" xs="10" offset-xs="1">
+        <div class="text-h3 text-center">
+          WMMBA Trail Conditions
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row>
+    <v-col cols="12" md="4" offset-md="4" xs="10" offset-xs="1">
+      <div class="text-h5 text-center">
+        Main WMMBA Page
+      </div>
+      <div class="text-h5 text-center">
+        Current Trail Conditions
+      </div>
+    </v-col>
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+}
+</script>
+
+<style scoped>
+
+</style>
